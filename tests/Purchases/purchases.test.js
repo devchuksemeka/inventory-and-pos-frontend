0b686183def8604fec60e0fcd82ba07abf493506
Miracle Okafor <miracleokafor@christians-MacBook-Pro.local>
2019-12-20 11:21:01 +0100

    added sample tests

---
+++ tests/Purchases/purchases.test.js
@@ -0,0 +1,4 @@
+const {sum} = require("../sample")
+test('Product Sum', () => {
+    expect(sum(1,2)).toBe(3);
+})
---
+++ tests/Sales/sales.test.js
@@ -0,0 +1,4 @@
+const {sum} = require("../sample")
+test('Sales Sum', () => {
+    expect(sum(1,2)).toBe(3);
+})
